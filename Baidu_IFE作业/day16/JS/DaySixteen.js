1b4a48f6c03e8259717a67ebd1030d2142762937
LoveCodeLiu <lovecodeliu@local.com>
2019-08-10 11:22:56 +0800

    day17 To day18

--- Baidu_IFE作业/day16/JS/DaySixteen.js
+++
@@ -1,6 +0,0 @@
-document.getElementById("hello").onclick = function () {
-    document.getElementById("hello-text").innerHTML  = "你好啊";
-};
-document.getElementById("favorite").onclick = function () {
-    document.getElementById("favorite-text").innerHTML  = "篮球，音乐，敲代码";
-};
